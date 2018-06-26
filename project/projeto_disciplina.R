48fe9fb298b94f85c306c4ec1a46736f4ebcef19
Luciano Silveira <lucianofsilveira@gmail.com>
2018-06-26 23:47:40 +0000

    Backup Projeto Final 1

--- project/projeto_disciplina.R
+++ project/projeto_disciplina.R
@@ -12,44 +12,184 @@ insta_products <- read_csv( "project/order_products_instacart.csv" ) # Produtos 
 
 #1 # Quantos dos produtos do cadastro nunca foram comprados?
 
+product_id <- products$product_id
 
-#2 # Crie um dataframe com os dados combinados de produtos, corredores e departamentos. 
+insta_products %>%
+    group_by(product_id) %>%
+    summarise(count = n()) %>%
+    ungroup() %>%
+    pull(product_id) -> orders_product_id
+
+notordered_product_id <- setdiff(product_id,orders_product_id)
+
+paste(length(notordered_product_id),'produtos nunca foram comprados.')
+
+
+#2 # Crie um dataframe com os dados combinados de produtos, corredores e departamentos.
+
+products %>%
+    inner_join(aisles, by = 'aisle_id') %>%
+    inner_join(departments, by = 'department_id') -> products_aisles_departments
 
 
 #3 # Quais as 10 combinações corredor + departamento que possuem mais produtos cadastrados? Use o dataframe da atividade #2.
 
+products_aisles_departments %>%
+    group_by(aisle_id, aisle, department_id, department) %>%
+    summarise(count = n()) %>%
+    arrange(desc(count)) %>%
+    head(count, n = 10) %>%
+    ungroup() -> top10_products_aisles_departments
 
 #4 # Qual o percentual de pedidos que possuem algum produto dos pares 'corredor + departamento' da atividade anterior?
 
+insta_products %>%
+    inner_join(products, by = 'product_id') %>%
+    inner_join(top10_products_aisles_departments, by = c('aisle_id', 'department_id')) %>%
+    group_by(order_id) %>%
+    summarise(count = n()) %>%
+    ungroup() %>%
+    pull(order_id) -> orders_top10_products_aisles_departments
+
+insta_products %>%
+    group_by(order_id) %>%
+    summarise(count = n()) %>%
+    ungroup() %>%
+    pull(order_id) -> orders_order_id
+
+perc_orders_aisles_departments <- (length(orders_top10_products_aisles_departments) * 100) / length(orders_order_id)
 
 #5 # Crie um novo dataframe de produtos em pedidos retirando aqueles produtos que não estão categorizados (usar resultado das atividades 3 e 4)
 
+insta_products %>%
+    inner_join(products, by = 'product_id') %>%
+    inner_join(top10_products_aisles_departments, by = c('aisle_id', 'department_id'))%>%
+    filter(aisle != 'missing' & department != 'missing') %>%
+    select(order_id, product_id) -> categorized_insta_products
 
 #6 # Crie um dataframe que combine todos os dataframes através das suas chaves de ligação. Para produtos de pedidos, use o dataframe da atividade 4
-   # Transforme as variáveis user_id, department e aisle em factor
-   # Transforme a variável order_hour_of_day em um factor ordenado (ordered)
 
-   # Este dataframe deverá ser utilizado em todas as atividades seguintes
+# Transforme as variáveis user_id, department e aisle em factor
+# Transforme a variável order_hour_of_day em um factor ordenado (ordered)
 
+# Este dataframe deverá ser utilizado em todas as atividades seguintes
+
+insta_products %>%
+    inner_join(products, by = 'product_id') %>%
+    inner_join(top10_products_aisles_departments, by = c('aisle_id', 'department_id')) %>%
+    inner_join(insta_orders, by = 'order_id') %>%
+    select(order_id,
+           user_id,
+           order_number,
+           order_dow,
+           order_hour_of_day,
+           days_since_prior_order,
+           product_id,
+           product_name,
+           aisle_id,
+           aisle,
+           department_id,
+           department) %>%
+    mutate(user_id = factor(user_id),
+           department = factor(department),
+           aisle = factor(aisle),
+           order_hour_of_day = ordered(order_hour_of_day)) -> top10_insta_orders
+
+summary(top10_insta_orders)
 
 #7 # Identifique os 5 horários com maior quantidade de usuários distintos que fizeram pedidos
 
+top10_insta_orders %>%
+    group_by(order_hour_of_day) %>%
+    summarise(count_user_id = n_distinct(user_id)) %>%
+    arrange(desc(count_user_id)) %>%
+    head(n = 5) %>%
+    ungroup() -> top5_hour_of_day
 
 #8 # Quais os 15 produtos mais vendidos nestes 5 horários? Identifique os produtos e a quantidade total nestes horários (total geral, não por hora)
 
+top10_insta_orders %>%
+    inner_join(top5_hour_of_day, by = 'order_hour_of_day') %>%
+    group_by(product_id, product_name) %>%
+    summarise(qtd = n()) %>%
+    arrange(desc(qtd)) %>%
+    head(15) -> top5_products
 
 #9 # Calcule a média de vendas por hora destes 15 produtos ao longo do dia,
    # e faça um gráfico de linhas mostrando a venda média por hora destes produtos. 
    # Utilize o nome do produto para legenda da cor da linha.
-   # Você consegue identificar algum produto com padrão de venda diferente dos demais? 
+   # Você consegue identificar algum produto com padrão de venda diferente dos demais?
 
+top5_products %>%
+    inner_join(top10_insta_orders, by = 'product_id') %>%
+    inner_join(products, by = 'product_id') %>%
+    group_by(product_id, product_name, order_hour_of_day, order_dow) %>%
+    summarise(qtd_hora = n()) %>%
+    ungroup() %>%
+    group_by(product_id, product_name, order_hour_of_day) %>%
+    summarise(media_qtd_hora = mean(qtd_hora)) %>%
+    ungroup()  %>% View() -> orders_product_x_hour
+
+library(ggplot2)
+
+ggplot(data = orders_product_x_hour,
+       aes(x = order_hour_of_day,
+           y = media_qtd_hora,
+           group = product_name)
+       ) +
+        geom_line(aes(color = product_name)) +
+        geom_point(aes(color = product_name)) +
+        scale_y_continuous(breaks = seq(from = 0, to = 30, by = 2)) +
+        labs(x = 'Hora',
+             y = 'Quantidade',
+             title = 'Top 15 Produtos por Hora',
+             colour = 'Produto')
+
+    #O produto Organic Whole String Cheese teve um pico de compras às 16h bem mais alto que qualquer outro produto.
 
 #10 # Calcule as seguintes estatísticas descritivas sobre a quantidade de pedidos por dia, para cada hora do dia. O resultado final deve ser exibido para cada hora do dia:
     # Média, Desvio Padrão, Mediana, Mínimo e Máximo
-    # Considerando os valores calculados, você acredita que a distribuição por hora é gaussiana? 
+    # Considerando os valores calculados, você acredita que a distribuição por hora é gaussiana?
+
+top10_insta_orders %>%
+    group_by(order_dow, order_hour_of_day) %>%
+    summarise(qtd_hora = n_distinct(order_id)) %>%
+    group_by(order_hour_of_day) %>%
+    summarise(media = mean(qtd_hora),
+              desvio_padrao = sd(qtd_hora),
+              mediana = median(qtd_hora),
+              minimo = min(qtd_hora),
+              maximo = max(qtd_hora)) %>%
+    ungroup()
+
+    #A distribuição é gaussiana, pois o volume de compras vai aumentando à medida que as horas passam,
+    #chega em um pico máximo (por volta das 14h) e depois começa a diminuir novamente.
 
 
 #11 # Faça um gráfico da média de quantidade de produtos por hora, com 1 desvio padrão para cima e para baixo em forma de gráfico de banda
+
+top10_insta_orders %>%
+    group_by(order_hour_of_day, product_id) %>%
+    summarise(qtd_hora = n(),
+              sd_low = mean(qtd_hora) - 1 * sd(qtd_hora),
+              sd_high = mean(qtd_hora) + 1 * sd(qtd_hora)) %>%
+    ungroup() %>%
+    #group_by(order_hour_of_day) %>%
+    #summarise(sd_low = mean(qtd_hora) - 1 * sd(qtd_hora),
+     #         sd_high = mean(qtd_hora) + 1 * sd(qtd_hora)) %>%
+    #ungroup() %>%
+    
+    ggplot( aes( x = order_hour_of_day, y = quantidade, ymin = sd_low, ymax = sd_high )) +
+    geom_ribbon(fill = "lightgray", alpha = 0.5) + 
+    geom_jitter(alpha = .2, height = 0, width = 0.3) +
+    #scale_x_continuous( breaks = 2005:2017 ) +
+    #scale_y_continuous( breaks = seq(from = -10, to = 80, by = 5 )) +
+    labs( x = "Ano de filmagem"
+          , y = "Quantidade de Línguas"
+          , title = "Evolução da quantidade de línguas por vídeo ao longo dos anos"
+          , subtitle = "Período considerado somente a partir de 2005. Dados ajustados para mínimo de 1 língua por apresentação.\n A faixa cinza correponde ao intervalo de 2 desvios padrão acima e abaixo da média, calculados ano a ano."
+          , caption = "Dados de TED Talks de https://www.kaggle.com/rounakbanik/ted-talks/data") +
+    theme_bw()
 
 
 #12 # Visualize um boxplot da quantidade de pedidos por hora nos 7 dias da semana. O resultado deve ter order_dow como eixo x.
@@ -69,6 +209,8 @@ insta_products <- read_csv( "project/order_products_instacart.csv" ) # Produtos 
 
 #17 # O vetor abaixo lista todos os IDs de bananas maduras em seu estado natural.
     # Utilizando este vetor, identifique se existem pedidos com mais de um tipo de banana no mesmo pedido.
+
+#bananas <- c(24852, 13176, 39276, 37067, 29259)
 
 
 #18 # Se existirem, pedidos resultantes da atividade 17, conte quantas vezes cada tipo de banana aparece nestes pedidos com mais de um tipo de banana.
